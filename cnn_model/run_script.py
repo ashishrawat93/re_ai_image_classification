b6dd033891f286e650e3a320ae2a6a0beef29c90
ASHISH RAWAT <puneashish@yahoo.co.in>
2018-05-06 22:55:00 -0400

    py script to run the classifier

---
+++ cnn_model/run_script.py
@@ -0,0 +1,50 @@
+import subprocess
+import os
+import numpy as np
+# python -m scripts.label_image     --graph=tf_files/retrained_graph.pb      --image=image_14.jpg
+
+w_dir='/Users/ashishrawat/Desktop/tensorflow_local'
+
+test_data = '/Users/ashishrawat/Desktop/tensorflow_local/test/'
+
+train_labels = os.listdir(test_data)
+train_labels.sort()
+print(train_labels)
+train_labels = train_labels[1:]
+images_per_class=24
+
+tf_test_y = []
+tf_test_y_pred = []
+
+g_cnt = 0
+
+for idx,folder in enumerate(train_labels):
+    print("folder:",folder)
+    dir = os.path.join(test_data, folder)
+    for x in range(1, images_per_class + 1):
+        print("image_"+str(x)+" : ",end=" ")
+        img = dir + "/image_"+str(x)+".jpg"
+        p = subprocess.Popen('python -m scripts.label_image     --graph=tf_files/retrained_graph.pb      --image='+img, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cnt=0
+        y_hat=None
+        for line in p.stdout.readlines():
+            if cnt ==6:
+                str_line = line.decode("utf-8")
+                ls = str_line.split(" ")
+                print(ls[0])
+                y_hat = str(ls[0])
+            # print(cnt, " : ", line)
+            cnt+=1
+        print( "y_hat: ",y_hat,"  ",end=" ")
+        tf_test_y.append(train_labels.index(folder))
+        tf_test_y_pred.append(train_labels.index(y_hat))
+
+        print(tf_test_y[g_cnt]," | ",tf_test_y_pred[g_cnt])
+        g_cnt+=1
+
+
+tf_test_y_pred = np.array(tf_test_y_pred)
+tf_test_y = np.array(tf_test_y)
+np.save("tensor_labels",tf_test_y)
+np.save("tensor_pred",tf_test_y_pred)
+np.save("tensor_training_labels",np.array(train_labels))
