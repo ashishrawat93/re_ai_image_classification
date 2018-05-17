b89a976f191b8fec718f5f00b374baa3ebabe120
ashishrawat93 <puneashish@yahoo.co.in>
2018-05-17 19:53:41 -0400

    merged the old folder to the newly pulled in

--- cnn_model/tensor_confusion.py
+++ cnn_model/tensor_confusion.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 
 y_hat = np.load("random_tensor_pred.npy")
@@ -8,7 +11,9 @@ y = np.load("random_tensor_labels.npy")
 
 m = abs(y_hat-y)
 m[m!=0] = 1
-print("Accuracy is : "(y.shape - m.sum())/y.shape)
+print((y.shape - m.sum())/y.shape)
+
+print(y_hat.shape,y.shape)
 
 #y_pred = cross_val_predict(model,trainDataGlobal,trainLabelsGlobal,cv=kfold)
 conf_mat = confusion_matrix(y,y_hat)
